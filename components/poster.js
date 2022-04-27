d1c851ab386e208383e8031fe8fbc2fc160384d9
ulldecorb <ausias.bertran@gmail.com>
2022-04-27 16:21:24 +0200

    Add text entity

--- components/poster.js
+++ components/poster.js
@@ -1,10 +1,21 @@
-// required('aframe');
+require('aframe');
 
-AFRAME.registerComponent('poster', {
-    schema: {type: 'string'},
-  
-    init: function () {
-      var stringToLog = this.data;
-      console.log(stringToLog);
-    }
+AFRAME.registerComponent('log', {
+  schema: {type: 'string'},
+
+  init: function () {
+    var stringToLog = this.data;
+    console.log(stringToLog);
+  }
 });
+
+AFRAME.registerComponent('foo', {
+  init: function () {
+    console.log(this.el.sceneEl);  // Reference to the scene element.
+  }
+});
+
+var sceneEl = document.querySelector('a-scene');
+console.log(sceneEl.querySelector('#redBox'));
+// const textEl = sceneEl.querySelector('a-text');
+// console.log(textEl.outerHTML())
